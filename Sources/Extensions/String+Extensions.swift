8e7f8eef94a133139531698fd2c295870024ca98
VG <15960525+brillcp@users.noreply.github.com>
2025-11-19 16:55:16 +0100

    Adds SHA string validation
    Adds a function to validate if a string is a valid SHA hash based on its length and character set.

--- Sources/Extensions/String+Extensions.swift
+++ Sources/Extensions/String+Extensions.swift
@@ -4,4 +4,9 @@ extension String {
     public var shortHash: String {
         String(prefix(6))
     }
+
+    public var isValidSHA: Bool {
+        let hex = CharacterSet(charactersIn: "0123456789abcdef")
+        return count == 40 && CharacterSet(charactersIn: self).isSubset(of: hex)
+    }
 }
