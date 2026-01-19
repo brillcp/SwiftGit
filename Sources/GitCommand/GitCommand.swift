776aa33d60da8479456153c489f43b306a691e24
VG <15960525+brillcp@users.noreply.github.com>
2026-01-19 17:28:59 +0100

    Removes pager from git log command
    Removes the `--no-pager` option from the git log command.
    
    This prevents `git log` from using a pager, which is not needed in this context and simplifies the output.

--- Sources/GitCommand/GitCommand.swift
+++ Sources/GitCommand/GitCommand.swift
@@ -89,8 +89,7 @@ extension GitCommand {
                 "log",
                 "--all",
                 "--topo-order",
-                "--no-pager",
-                "--no-show-signature",  
+                "--no-show-signature",
                 "-n", "\(limit)",
                 "--format=%H%x00%P%x00%T%x00%an%x00%ae%x00%at%x00%cn%x00%ce%x00%ct%x00%s%x00%b"
             ]
