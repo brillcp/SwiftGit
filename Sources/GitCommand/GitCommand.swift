8d9b86aa38bfda74c263f10bb9fb777a61952a46
VG <15960525+brillcp@users.noreply.github.com>
2026-01-23 23:03:57 +0100

    Adds `--first-parent` to `git diff-tree` command
    Ensures `git diff-tree` only considers the first parent when following commits, which is often desirable for simplified history traversal and change analysis.

--- Sources/GitCommand/GitCommand.swift
+++ Sources/GitCommand/GitCommand.swift
@@ -208,7 +208,7 @@ extension GitCommand {
             args.append(path)
             return args
         case .diffTree(let commitId):
-            return ["diff-tree", "-z", "--no-commit-id", "--name-status", "-r", "-M", commitId]
+            return ["diff-tree", "-z", "--no-commit-id", "--name-status", "-r", "-M", "--first-parent", commitId]
         case .diffCommits(let from, let to, let path):
             return ["diff", from, to, "--", path]
         case .diffFromEmpty(let to, let path):
